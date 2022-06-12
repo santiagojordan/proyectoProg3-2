186650774bfd36998842fc8f1ad8534e287c28b8
santiagojordan <sjordan@udesa.edu.ar>
2022-06-12 16:56:21 -0300

    Login y Registro

--- 2proyecto/src/screens/login.js
+++ 2proyecto/src/screens/login.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
+import {View, StyleSheet, TouchableOpacity, Text, TextInput} from 'react-native';
 
 class Login extends Component {
-    render() {
+    /*render() {
         return (
             <View>
                 <TouchableOpacity onPress={ ()=> this.props.navigation.navigate('Registro')}>
@@ -12,9 +12,71 @@ class Login extends Component {
                 </TouchableOpacity>
             </View>
         );
+    } */
+
+    constructor(props){
+        super(props)
+        this.state={
+            email: '',
+            password: '',
+        }
+    }
+
+    render(){
+        //Falta implementar for de login y el método que viene de mainNavigation 
+        return(
+                <View style={styles.container}>
+                <Text style={styles.title}>Login</Text>
+                <TextInput 
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='Email'
+                    onChangeText={text => this.setState({ email: text})}
+                />
+                <TextInput 
+                    style={styles.field}
+                    keyboardType='default'
+                    placeholder='password'
+                    secureTextEntry={true}
+                    onChangeText={text => this.setState({ password: text})}
+                />
+                <TouchableOpacity style={styles.button} onPress={()=>this.props.route.params.login(this.state.email, this.state.password)}>
+                    <Text style={ styles.buttonText}>Ingresar</Text>
+                </TouchableOpacity>   
+                 <TouchableOpacity onPress={ ()=>this.props.navigation.navigate('Registro') }>
+                        <Text>No tengo cuenta</Text>
+                 </TouchableOpacity>
+            
+            </View>
+        )
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        paddingHorizontal:10,
+        marginTop: 10
+    },
+    title:{
+        marginBottom:20
+    },
+    field:{
+        borderColor: '#dcdcdc',
+        borderWidth: 1,
+        borderRadius: 2,
+        padding:3,
+        marginBottom:8
+
+    },
+    button: {
+        borderRadius: 2,
+        padding:3,
+        backgroundColor: 'green',
+    },
+    buttonText:{
+        color: '#fff'
+    }
+})
+
 
 export default Login;
