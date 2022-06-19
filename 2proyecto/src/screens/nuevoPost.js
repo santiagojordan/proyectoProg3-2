6d252134e5d686642234f18934495ad5e2f24a21
Tomas Jarsun <toto@Totos-Air.localdomain>
2022-06-19 17:10:15 -0300

    avances

--- 2proyecto/src/screens/nuevoPost.js
+++ 2proyecto/src/screens/nuevoPost.js
@@ -45,58 +45,6 @@ class NewPost extends Component{
     }
 
 
-    render(){
-        return(
-            <View style={styles.container}>
-            {
-                this.state.showCamera ?
-                    <MyCamera onImageUpload={url => this.onImageUpload(url)}/> 
-                :
-                <View style={styles.container}>
-                    <Text style={styles.title}>Nuevo Post</Text>
-                    <TextInput 
-                        style={styles.field}
-                        keyboardType='default'
-                        placeholder='description'
-                        onChangeText={text => this.setState({ description: text})}
-                        multiline
-                    />
-                    <TouchableOpacity style={styles.button} onPress={()=>this.guardarPost()}>
-                        <Text style={styles.buttonText}>Guardar Post</Text>
-                    </TouchableOpacity>               
-                </View>
 
-            }
-            </View>
-        )
-    }
-
-}
-
-const styles = StyleSheet.create({
-    container:{
-        paddingHorizontal:10,
-        marginTop: 10,
-        height:'100%'
-    },
-    title:{
-        marginBottom:20
-    },
-    field:{
-        borderColor: '#dcdcdc',
-        borderWidth: 1,
-        borderRadius: 2,
-        padding:3,
-        marginBottom:8
-    },
-    button: {
-        borderRadius: 2,
-        padding:3,
-        backgroundColor: 'green',
-    },
-    buttonText:{
-        color: '#fff'
-    }
-})
 
 export default NewPost;
